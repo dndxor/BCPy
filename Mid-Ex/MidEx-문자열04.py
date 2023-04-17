766fc84938e57952ce3ea5bb4fa50ec480feb996
dndxor <127068465+dndxor@users.noreply.github.com>
2023-04-17 18:22:19 +0900

    20230417

--- Mid-Ex/MidEx-문자열04.py
+++ Mid-Ex/MidEx-문자열04.py
@@ -11,8 +11,8 @@ for i in range(slen):
     for j in range(slen-i-1, slen):
         print("\b", end='')
 
-#[패턴] 출력되는 글자 수 패턴
-for i in range(slen-1, 0-1, -1):
+#문자열 배열로 출력(우에서 좌로 흐름 이동)
+for i in range(slen):
     for j in range(i, slen):
         print("%c" %str[j], end='')
     time.sleep(0.5)
